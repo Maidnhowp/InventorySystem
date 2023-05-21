4f3e81da3d8eec9761d4777028d0663f61f06159
boost <boost@boosts-MacBook-Air.local>
2023-05-21 17:39:59 +0700

    login and register

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    include('server.php');
+    session_start();
+
+    if(isset($_GET['logout'])) {
+        unset($_SESSION['username']);
+        session_destroy();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php if(!isset($_SESSION['username'])) { ?>
+        <a href="register.php"><button>sign up</button></a>
+        <a href="login.php"><button>sign in</button></a>
+    <?php } else { ?>
+        <a href="index.php?logout='1'"><button>log out</button></a>
+    <?php } ?>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,88 @@
+<?php
+    include('server.php');
+    session_start();
+
+    if(isset($_POST['register'])) {
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+        $password2 = mysqli_real_escape_string($conn, $_POST['password2']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        
+        $error = "";
+
+        if(!isset($_SESSION['filled'])) {
+            $_SESSION['filled'] = array('username'=>$username, "email"=>$email);
+        } else {
+            $_SESSION['filled']['username'] = $username;
+            $_SESSION['filled']['email'] = $email;
+        }
+
+        //all form is not empty 
+        if(!empty($username) and !empty($password) and !empty($password2) and !empty($email)) {        
+            $sql = "SELECT * FROM user where username = '$username' or email = '$email'";
+            $query = mysqli_query($conn, $sql);
+
+            if($password != $password2) {
+                $error = "Password and Confirm Password doesn't match";
+            }
+
+            if(mysqli_num_rows($query) > 0) {
+                $error = "This username or email is already used";
+                unset($_SESSION['filled']);
+            }
+
+            if($error == "") {
+                unset($_SESSION['filled']);
+                $sql = "INSERT INTO user(username, password, email) VALUES('$username', '$password', '$email')";
+                mysqli_query($conn, $sql);
+                
+                $_SESSION['username'] = $username;
+                header('location: index.php');
+            }
+
+        } else {
+            $error = "Please fill all of the form";
+        }
+    } else {
+        unset($_SESSION['filled']);
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <div>
+        <form action="register.php" method="post">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" value="<?= $_SESSION['filled']['username'] ?>">
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" value="<?= $_SESSION['filled']['email'] ?>">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password">
+            </div>
+            <div>
+                <label for="password2">Confirm Password</label>
+                <input type="password" name="password2">
+            </div>
+            <button type="submit" name="register">Register</button>
+        </form>
+        <?php
+            if(!empty($error)) {
+                echo "<p>$error</p>";
+            } 
+        ?>
+    </div>
+</body>
+</html>
