0085b751abe7013f62f6dd45b5c21543d1be385c
Maidnhowp <118268388+Maidnhowp@users.noreply.github.com>
2023-05-26 00:35:39 +0700

    Add files via upload

---
+++ test/test1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/slider.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+    <div class="wrapper">
+        <ul class="carousel">
+            <li class="card" id="card-1">
+                <img src="https://cdn.pixabay.com/photo/2016/11/23/13/48/beach-1852945_1280.jpg" alt="">
+                <H2>SEA1</H2>
+                <span>Lorem ipsum</span>
+            </li>
+            <li class="card" id="card-2">
+                <img src="https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581_640.jpg" alt="">
+                <H2>SEA2</H2>
+                <span>Lorem ipsum</span>
+            </li>
+            <li class="card" id="card-3">
+                <img src="https://cdn.pixabay.com/photo/2017/01/20/00/30/maldives-1993704_640.jpg" alt="">
+                <H2>SEA3</H2>
+                <span>Lorem ipsum</span>
+            </li>
+            <li class="card" id="card-4">
+                <img src="https://cdn.pixabay.com/photo/2016/10/18/21/22/beach-1751455_640.jpg" alt="">
+                <H2>SEA4</H2>
+                <span>Lorem ipsum</span>
+            </li>
+            <li class="card" id="card-5">
+                <img src="https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_640.jpg" alt="">
+                <H2>SEA5</H2>
+                <span>Lorem ipsum</span>
+            </li>
+        </ul>
+    </div>
+    <div class="slider-nav">
+            <a href="#card-1"></a>
+            <a href="#card-4"></a>
+            <a href="#card-5"></a>
+    </div>
+</body>
+</html>
