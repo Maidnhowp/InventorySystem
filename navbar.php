37bef242fcfeced70bfc9f6cc74d9363a00ccfc0
Maidnhowp <118268388+Maidnhowp@users.noreply.github.com>
2023-06-01 23:58:54 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="../test.css">
+<link rel="stylesheet" href="css/navStyle.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <nav class="navbar">
     <div class="containner">
